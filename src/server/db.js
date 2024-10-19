492ed8e1d357c4a72add1816f48347b04ca937d7
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2024-10-18 22:37:04 -0700

    feat: get and post routes created

--- src/server/db.js
+++ src/server/db.js
@@ -1,12 +1,14 @@
 import pg from "pg";
-import "dotenv/config";
+import "dotenv/config"
 
 const db = new pg.Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  passowrd: process.env.DB_PASSOWRD,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
+
+
 
 export default db
--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,37 @@
-import express from "express"
-import db from "./db.js"
+import express from "express";
+import db from "./db.js";
+import { createUser, getUser } from "./queries/userQueries.ts";
 
-const PORT = process.env.PORT ?? 8000
-const app = express()
+const PORT = process.env.PORT ?? 8000;
+const app = express();
 
+app.use(express.json());
 
-app.get('/', (req,res) => {
-  res.send('testing')
-})
+// Route to get user
+app.get("/api/user", async (req, res) => {
+  const { user_id } = req.body;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  try {
+    const user = await getUser({ user_id });
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error getting user:", error);
+    res.status(500);
+  }
+});
 
+// Route to create a user
+app.post("/api/user", async (req, res) => {
+  const { user_id, email, first_name, last_name, major, pronouns } = req.body;
+  const userInfo = { user_id, email, first_name, last_name, major, pronouns };
 
+  try {
+    const newUser = await createUser(userInfo);
+    res.status(200).json(newUser);
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
