ef472cc7761672e285109e10068742162c274769
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2024-10-25 20:21:11 -0700

    feat: desktop nav bar completed

--- src/app/(hasNavbar)/layout.tsx
+++ src/app/(hasNavbar)/layout.tsx
@@ -24,7 +24,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        {/* <Navbar /> */}
+        <Navbar />
         <MobileTopBar />
         {children}
         <MobileNavbar />
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,47 +1,105 @@
+"use client";
+
 import Logo from "../logo/logo";
-import Home from "../../assets/icons/home.svg";
-import Clubs from "../../assets/icons/clubs.svg";
-import Friends from "../../assets/icons/friends.svg";
-import Messages from "../../assets/icons/messages.svg";
+import Home from "../../assets/icons/home";
+import Clubs from "../../assets/icons/clubs";
+import Friends from "../../assets/icons/friends";
+import Messages from "../../assets/icons/messages";
+import Post from "../../assets/icons/post";
 import Search from "../../assets/icons/search.svg";
 import defaultProfile from "../../assets/images/defaultProfile.png";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./navbar.module.scss";
+import { usePathname } from "next/navigation";
+import { auth } from "@/auth";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const pathname = usePathname();
+  const session = await auth();
+  const userId = session?.user?.id;
   return (
     <nav className={styles.navbar}>
       <div className={styles.container}>
-        <Logo />
+        <Link href={"/home"} className={styles.logoContainer}>
+          <Logo className={styles.logo} />
+        </Link>
         <ul>
           <li>
-            <Link className={styles.icon} href={"/home"}>
-              <Image src={Home} width={0} height={0} alt="home" />
+            <Link
+              className={`${styles.icon} ${
+                pathname == "/home" ? styles.active : ""
+              }`}
+              href={"/home"}
+            >
+              <Home />
               <p>Home</p>
             </Link>
           </li>
           <li>
-            <Link className={styles.icon} href={"/clubs"}>
-              <Image src={Clubs} width={0} height={0} alt="clubs" />
+            <Link
+              className={`${styles.icon} ${
+                pathname == "/clubs" ? styles.active : ""
+              }`}
+              href={"/clubs"}
+            >
+              <Clubs />
               <p>Clubs</p>
             </Link>
           </li>
           <li>
-            <Link className={styles.icon} href={"/friends"}>
-              <Image src={Friends} width={0} height={0} alt="friends" />
+            <Link
+              className={`${styles.icon} ${
+                pathname == "/post" ? styles.active : ""
+              }`}
+              href={"/post"}
+            >
+              <Post />
+              <p>Post</p>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`${styles.icon} ${
+                pathname == "/friends" ? styles.active : ""
+              }`}
+              href={"/friends"}
+            >
+              <Friends />
               <p>Friends</p>
             </Link>
           </li>
           <li>
-            <Link className={styles.icon} href={"/messages"}>
-              <Image src={Messages} width={0} height={0} alt="messages" />
+            <Link
+              className={`${styles.icon} ${
+                pathname == "/messages" ? styles.active : ""
+              }`}
+              href={"/messages"}
+            >
+              <Messages />
               <p>Messages</p>
             </Link>
           </li>
         </ul>
-        <div>
-          <Image src={Search} width={0} height={0} alt="search" />
+        <div className={styles.profileSearchContainer}>
+          <Link href={"/search"}>
+            <Image
+              className={styles.searchIcon}
+              src={Search}
+              width={0}
+              height={0}
+              alt="search"
+            />
+          </Link>
+          <Link className={styles.profile} href={`/profile/${userId}`}>
+            <Image
+              src={defaultProfile}
+              width={0}
+              height={0}
+              alt="profile"
+              className={styles.profileImg}
+            />
+          </Link>
         </div>
       </div>
     </nav>
