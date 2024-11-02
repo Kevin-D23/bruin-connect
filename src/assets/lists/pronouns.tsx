7e4f0bdccaa76f61d2bc3f4b902b4b33b6fb0b56
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2024-11-01 19:22:33 -0700

    feat: profile cropping complete

---
+++ src/assets/lists/pronouns.tsx
@@ -0,0 +1,14 @@
+export const pronounOptions = [
+    { value: "he_him", label: "He/Him" },
+    { value: "she_her", label: "She/Her" },
+    { value: "they_them", label: "They/Them" },
+    { value: "he_they", label: "He/They" },
+    { value: "she_they", label: "She/They" },
+    { value: "xe_xem", label: "Xe/Xem" },
+    { value: "ze_zie_hir", label: "Ze/Zie/Hir" },
+    { value: "ey_em", label: "Ey/Em" },
+    { value: "ve_ver", label: "Ve/Ver" },
+    { value: "fae_faer", label: "Fae/Faer" },
+    { value: "per_per", label: "Per/Per" },
+    { value: "null", label: "None" },
+  ];
--- src/components/signInCard/signInCard.tsx
+++ src/components/signInCard/signInCard.tsx
@@ -5,241 +5,29 @@ import signInImage from "../../assets/images/signInImage.png";
 import Logo from "@/components/logo/logo";
 import { SignIn } from "@/app/api/auth/actions";
 import google from "../../assets/icons/google.png";
-import { useEffect, useState } from "react";
+import {
+  MutableRefObject,
+  SyntheticEvent,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import Select, { SingleValue } from "react-select";
 import { SignOut } from "@/app/api/auth/actions";
 import { useRouter } from "next/navigation";
-import ReactCrop, { centerCrop, Crop, makeAspectCrop } from "react-image-crop";
+import ReactCrop, {
+  centerCrop,
+  convertToPixelCrop,
+  Crop,
+  makeAspectCrop,
+  PixelCrop,
+} from "react-image-crop";
 import "react-image-crop/src/ReactCrop.scss";
-
-const majorOptions = [
-  { value: "undeclared", label: "Undeclared" },
-  { value: "aerospace_engineering", label: "Aerospace Engineering" },
-  { value: "african_american_studies", label: "African American Studies" },
-  {
-    value: "african_and_middle_eastern_studies",
-    label: "African and Middle Eastern Studies",
-  },
-  { value: "american_indian_studies", label: "American Indian Studies" },
-  {
-    value: "american_literature_and_culture",
-    label: "American Literature and Culture",
-  },
-  {
-    value: "ancient_near_east_and_egyptology",
-    label: "Ancient Near East and Egyptology",
-  },
-  { value: "anthropology", label: "Anthropology" },
-  { value: "architectural_studies", label: "Architectural Studies" },
-  { value: "art", label: "Art" },
-  { value: "art_history", label: "Art History" },
-  { value: "asian_american_studies", label: "Asian American Studies" },
-  { value: "asian_humanities", label: "Asian Humanities" },
-  {
-    value: "asian_languages_and_linguistics",
-    label: "Asian Languages and Linguistics",
-  },
-  { value: "asian_religions", label: "Asian Religions" },
-  { value: "asian_studies", label: "Asian Studies" },
-  { value: "astrophysics", label: "Astrophysics" },
-  {
-    value: "atmospheric_and_oceanic_sciences",
-    label: "Atmospheric and Oceanic Sciences",
-  },
-  {
-    value: "atmospheric_and_oceanic_sciences_mathematics",
-    label: "Atmospheric and Oceanic Sciences/Mathematics",
-  },
-  { value: "biochemistry", label: "Biochemistry" },
-  { value: "bioengineering", label: "Bioengineering" },
-  { value: "biology", label: "Biology" },
-  { value: "biophysics", label: "Biophysics" },
-  { value: "business_economics", label: "Business Economics" },
-  {
-    value: "central_and_east_european_languages_and_cultures",
-    label: "Central and East European Languages and Cultures",
-  },
-  { value: "chemical_engineering", label: "Chemical Engineering" },
-  { value: "chemistry", label: "Chemistry" },
-  {
-    value: "chemistry_materials_science",
-    label: "Chemistry/Materials Science",
-  },
-  {
-    value: "chicana_and_chicano_studies",
-    label: "Chicana and Chicano Studies",
-  },
-  { value: "chinese", label: "Chinese" },
-  { value: "civil_engineering", label: "Civil Engineering" },
-  { value: "classical_civilization", label: "Classical Civilization" },
-  { value: "climate_science", label: "Climate Science" },
-  { value: "cognitive_science", label: "Cognitive Science" },
-  { value: "communication", label: "Communication" },
-  { value: "comparative_literature", label: "Comparative Literature" },
-  {
-    value: "computational_and_systems_biology",
-    label: "Computational and Systems Biology",
-  },
-  { value: "computer_engineering", label: "Computer Engineering" },
-  { value: "computer_science", label: "Computer Science" },
-  {
-    value: "computer_science_and_engineering",
-    label: "Computer Science and Engineering",
-  },
-  { value: "dance", label: "Dance" },
-  { value: "data_theory", label: "Data Theory" },
-  { value: "design_media_arts", label: "Design | Media Arts" },
-  { value: "disability_studies", label: "Disability Studies" },
-  {
-    value: "earth_and_environmental_science",
-    label: "Earth and Environmental Science",
-  },
-  {
-    value: "ecology_behavior_and_evolution",
-    label: "Ecology, Behavior, and Evolution",
-  },
-  { value: "economics", label: "Economics" },
-  {
-    value: "education_and_social_transformation",
-    label: "Education and Social Transformation",
-  },
-  { value: "electrical_engineering", label: "Electrical Engineering" },
-  { value: "english", label: "English" },
-  { value: "environmental_science", label: "Environmental Science" },
-  {
-    value: "european_language_and_transcultural_studies",
-    label: "European Language and Transcultural Studies",
-  },
-  { value: "film_and_television", label: "Film and Television" },
-  { value: "gender_studies", label: "Gender Studies" },
-  { value: "geography", label: "Geography" },
-  {
-    value: "geography_environmental_studies",
-    label: "Geography/Environmental Studies",
-  },
-  { value: "geology", label: "Geology" },
-  {
-    value: "geology_engineering_geology",
-    label: "Geology/Engineering Geology",
-  },
-  { value: "geophysics", label: "Geophysics" },
-  { value: "global_jazz_studies", label: "Global Jazz Studies" },
-  { value: "global_studies", label: "Global Studies" },
-  { value: "history", label: "History" },
-  { value: "human_biology_and_society", label: "Human Biology and Society" },
-  {
-    value: "individual_field_of_concentration",
-    label: "Individual Field of Concentration",
-  },
-  {
-    value: "international_development_studies",
-    label: "International Development Studies",
-  },
-  { value: "iranian_studies", label: "Iranian Studies" },
-  { value: "japanese", label: "Japanese" },
-  { value: "jewish_studies", label: "Jewish Studies" },
-  { value: "korean", label: "Korean" },
-  { value: "labor_studies", label: "Labor Studies" },
-  { value: "latin", label: "Latin" },
-  { value: "latin_american_studies", label: "Latin American Studies" },
-  { value: "linguistics", label: "Linguistics" },
-  {
-    value: "linguistics_and_anthropology",
-    label: "Linguistics and Anthropology",
-  },
-  {
-    value: "linguistics_and_asian_languages_and_cultures",
-    label: "Linguistics and Asian Languages and Cultures",
-  },
-  {
-    value: "linguistics_and_computer_science",
-    label: "Linguistics and Computer Science",
-  },
-  { value: "linguistics_and_english", label: "Linguistics and English" },
-  {
-    value: "linguistics_and_philosophy",
-    label: "Linguistics and Philosophy",
-  },
-  {
-    value: "linguistics_and_psychology",
-    label: "Linguistics and Psychology",
-  },
-  { value: "linguistics_and_spanish", label: "Linguistics and Spanish" },
-  { value: "linguistics_applied", label: "Linguistics, Applied" },
-  { value: "marine_biology", label: "Marine Biology" },
-  { value: "materials_engineering", label: "Materials Engineering" },
-  { value: "mathematics", label: "Mathematics" },
-  { value: "mathematics_applied", label: "Mathematics, Applied" },
-  {
-    value: "mathematics_applied_science",
-    label: "Mathematics/Applied Science",
-  },
-  { value: "mathematics_economics", label: "Mathematics/Economics" },
-  {
-    value: "mathematics_financial_actuarial",
-    label: "Mathematics, Financial Actuarial",
-  },
-  { value: "mathematics_for_teaching", label: "Mathematics for Teaching" },
-  {
-    value: "mathematics_of_computation",
-    label: "Mathematics of Computation",
-  },
-  { value: "mechanical_engineering", label: "Mechanical Engineering" },
-  {
-    value: "microbiology_immunology_and_molecular_genetics",
-    label: "Microbiology, Immunology, and Molecular Genetics",
-  },
-  { value: "middle_eastern_studies", label: "Middle Eastern Studies" },
-  {
-    value: "molecular_cell_and_developmental_biology",
-    label: "Molecular, Cell, and Developmental Biology",
-  },
-  { value: "music_composition", label: "Music Composition" },
-  { value: "music_education", label: "Music Education" },
-  { value: "music_industry", label: "Music Industry" },
-  { value: "musicology", label: "Musicology" },
-  { value: "nursing_prelicensure", label: "Nursing - Prelicensure" },
-  { value: "neuroscience", label: "Neuroscience" },
-  { value: "philosophy", label: "Philosophy" },
-  { value: "physics", label: "Physics" },
-  { value: "physiological_science", label: "Physiological Science" },
-  { value: "political_science", label: "Political Science" },
-  {
-    value: "portuguese_and_brazilian_studies",
-    label: "Portuguese and Brazilian Studies",
-  },
-  { value: "psychobiology", label: "Psychobiology" },
-  { value: "psychology", label: "Psychology" },
-  { value: "public_affairs", label: "Public Affairs" },
-  { value: "religion", label: "Religion" },
-  {
-    value: "russian_language_and_literature",
-    label: "Russian Language and Literature",
-  },
-  { value: "russian_studies", label: "Russian Studies" },
-  { value: "scandinavian", label: "Scandinavian" },
-  { value: "sociology", label: "Sociology" },
-  { value: "south_asian", label: "South Asian" },
-  { value: "spanish", label: "Spanish" },
-  { value: "statistics", label: "Statistics" },
-  { value: "theater", label: "Theater" },
-  { value: "world_arts_and_cultures", label: "World Arts and Cultures" },
-];
-
-const pronounOptions = [
-  { value: "he_him", label: "He/Him" },
-  { value: "she_her", label: "She/Her" },
-  { value: "they_them", label: "They/Them" },
-  { value: "he_they", label: "He/They" },
-  { value: "she_they", label: "She/They" },
-  { value: "xe_xem", label: "Xe/Xem" },
-  { value: "ze_zie_hir", label: "Ze/Zie/Hir" },
-  { value: "ey_em", label: "Ey/Em" },
-  { value: "ve_ver", label: "Ve/Ver" },
-  { value: "fae_faer", label: "Fae/Faer" },
-  { value: "per_per", label: "Per/Per" },
-  { value: "null", label: "None" },
-];
+import Upload from "@/assets/icons/upload";
+import { majorOptions } from "@/assets/lists/majorOptions";
+import { pronounOptions } from "@/assets/lists/pronouns";
+import Xmark from "@/assets/icons/Xmark";
+import Checkmark from "@/assets/icons/checkmark";
 
 // formats email to hidden email
 function formatEmail(email: String) {
@@ -283,7 +71,11 @@ export default function SignInCard({ pageName, userId, email }: PageProps) {
   const [crop, setCrop] = useState<Crop>();
   const [imageError, setImageError] = useState("");
   const ASPECT_RATIO = 1;
-  const MIN_DIMENSION = 150;
+  const MIN_DIMENSION = 100;
+  const [pictureConfirmed, setPictureConfirmed] = useState(false);
+  const imgRef = useRef<HTMLImageElement>(null);
+  const previewCanvasRef = useRef(null);
+  const [tempImg, setTempImg] = useState("");
 
   const router = useRouter();
 
@@ -364,24 +156,29 @@ export default function SignInCard({ pageName, userId, email }: PageProps) {
     if (!file) return;
 
     const reader = new FileReader();
-    reader.addEventListener("load", () => {
+    reader.addEventListener("load", (e) => {
+      const imageElement = document.createElement("img");
       const imgUrl = reader.result?.toString() || "";
-      setFormData((prevState) => ({
-        ...prevState,
-        profile_picture: imgUrl,
-      }));
+      imageElement.src = imgUrl;
+
+      // on image load, check if image meets dimension requirements
+      imageElement.addEventListener("load", (e: any) => {
+        if (imageError) setImageError("");
+        const { naturalWidth, naturalHeight } = e.currentTarget;
+        if (naturalHeight < MIN_DIMENSION || naturalWidth < MIN_DIMENSION) {
+          setImageError("** Image must be at least 150x150 pixels");
+          return setTempImg("");
+        }
+      });
+
+      setTempImg(imgUrl);
     });
     reader.readAsDataURL(file);
   }
 
   // set crop dimensions
-  function onImageLoad(e: any) {
+  function onImageLoad(e: SyntheticEvent<HTMLImageElement, Event>) {
     const { width, height, naturalWidth, naturalHeight } = e.currentTarget;
-    if (naturalHeight < MIN_DIMENSION || naturalWidth < MIN_DIMENSION) {
-      setImageError("** Image must be at least 150x150 pixels");
-      setFormData((prevState) => ({ ...prevState, profile_picture: "" }));
-      return;
-    }
 
     const crop = makeAspectCrop(
       {
@@ -393,7 +190,50 @@ export default function SignInCard({ pageName, userId, email }: PageProps) {
       height
     );
     const centeredCrop = centerCrop(crop, width, height);
-    setCrop(crop);
+    setCrop(centeredCrop);
+  }
+
+  // draws new canvas for cropped image
+  function setCanvasPreview(
+    image: HTMLImageElement | null,
+    canvas: HTMLCanvasElement | null,
+    crop: any
+  ) {
+    if (canvas && image) {
+      const ctx = canvas.getContext("2d");
+      if (!ctx) {
+        throw new Error("No 2d context");
+      }
+
+      const pixelRatio = window.devicePixelRatio;
+      const scaleX = image.naturalWidth / image.width;
+      const scaleY = image.naturalHeight / image.height;
+
+      canvas.width = Math.floor(crop.width * scaleX * pixelRatio);
+      canvas.height = Math.floor(crop.height * scaleY * pixelRatio);
+
+      ctx.scale(pixelRatio, pixelRatio);
+      ctx.imageSmoothingQuality = "high";
+      ctx.save();
+
+      const cropX = crop.x * scaleX;
+      const cropY = crop.y * scaleY;
+
+      ctx.translate(-cropX, -cropY);
+      ctx.drawImage(
+        image,
+        0,
+        0,
+        image.naturalWidth,
+        image.naturalHeight,
+        0,
+        0,
+        image.naturalWidth,
+        image.naturalHeight
+      );
+
+      ctx.restore();
+    }
   }
 
   return (
@@ -407,7 +247,7 @@ export default function SignInCard({ pageName, userId, email }: PageProps) {
       />
       <div className={styles.formContainer}>
         <Logo className={styles.logo} />
-
+        {/* Google Sign in  */}
         {pageName == "signIn" && (
           <div className={styles.btnContainer}>
             <button className={styles.googleBtn} onClick={() => SignIn()}>
@@ -416,6 +256,7 @@ export default function SignInCard({ pageName, userId, email }: PageProps) {
             </button>
           </div>
         )}
+        {/* User data registration */}
         {pageName == "register" && (
           <form onSubmit={handleSubmit} className={styles.form}>
             {registrationPage == 1 && (
@@ -491,31 +332,75 @@ export default function SignInCard({ pageName, userId, email }: PageProps) {
                 </a>
               </div>
             )}
+            {/* Profile picture registration */}
             {registrationPage == 2 && (
               <div className={styles.imgUpload}>
-                <div className={styles.imgBtnContainer}>
-                  <h2>Profile picture:</h2>
-                  <input type="file" id="uploadBtn" onChange={uploadFile} hidden/>
-                  <label htmlFor="uploadBtn">Upload</label>
-
-                </div>
+                {!tempImg && (
+                  <div className={styles.imgBtnContainer}>
+                    <input
+                      type="file"
+                      id="uploadBtn"
+                      onChange={uploadFile}
+                      hidden
+                    />
+                    <label htmlFor="uploadBtn">
+                      <Upload />
+                      Profile Picture
+                    </label>
+                  </div>
+                )}
                 {imageError && <p>{imageError}</p>}
 
-                {formData.profile_picture && (
-                  <ReactCrop
-                    crop={crop}
-                    circularCrop
-                    keepSelection
-                    aspect={ASPECT_RATIO}
-                    minWidth={MIN_DIMENSION}
-                    onChange={(pixelCrop, percentCrop) => setCrop(pixelCrop)}
-                  >
-                    <img
-                      src={formData.profile_picture}
-                      alt="upload"
-                      onLoad={(e) => onImageLoad(e)}
-                    />
-                  </ReactCrop>
+                {tempImg && !pictureConfirmed && (
+                  <div className={styles.cropImgContainer}>
+                    <ReactCrop
+                      crop={crop}
+                      circularCrop
+                      keepSelection
+                      aspect={ASPECT_RATIO}
+                      minWidth={MIN_DIMENSION}
+                      onChange={(pixelCrop, percentCrop) => setCrop(pixelCrop)}
+                    >
+                      <img
+                        src={tempImg}
+                        alt="upload"
+                        onLoad={(e) => onImageLoad(e)}
+                        ref={imgRef}
+                      />
+                    </ReactCrop>
+                    <div>
+                      <button type="button">
+                        <Xmark />
+                      </button>
+                      <button
+                        type="button"
+                        onClick={() => {
+                          if (imgRef.current && crop)
+                            setCanvasPreview(
+                              imgRef.current,
+                              previewCanvasRef.current,
+                              convertToPixelCrop(
+                                crop,
+                                imgRef.current.width,
+                                imgRef.current.height
+                              )
+                            );
+                        }}
+                      >
+                        <Checkmark />
+                      </button>
+                      {crop && (
+                        <canvas
+                          ref={previewCanvasRef}
+                          style={{
+                            height: 100,
+                            width: 100,
+                            display: "none",
+                          }}
+                        ></canvas>
+                      )}
+                    </div>
+                  </div>
                 )}
               </div>
             )}
