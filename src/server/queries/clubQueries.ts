459fba4fbb3b3d754d2ab805f11bb921781e0cc5
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2024-11-22 21:17:35 -0800

    feat: club queries added

--- src/server/queries/clubQueries.ts
+++ src/server/queries/clubQueries.ts
@@ -9,3 +9,49 @@
 // 		FOREIGN KEY("owner")
 // 			REFERENCES "user"(user_id)
 // );
+
+import db from "../db";
+
+type Club = {
+  club_id: String;
+  club_name: String;
+  description: String;
+  logo: String;
+};
+
+// Get user by id
+// Return:
+// {
+// club_id: String;
+// club_name: String;
+// description: String;
+// logo:String;
+// }
+export async function getClubById({ club_id }: Club) {
+  const query = `SELECT club_id, club_name, description, logo FROM "user" WHERE user_id = ($1)`;
+  const values = [club_id];
+
+  try {
+    const result = await db.query(query, values);
+    return result.rows[0];
+  } catch (error) {
+    console.error("Error getting club:", error);
+  }
+}
+
+// Creates a club instance
+export async function createClub({
+  club_id,
+  club_name,
+  description,
+  logo,
+}: Club) {
+  const query = `INSERT INTO club (club_id, club_name, description, logo) VALUES ($1, $2, $3, $4) RETURNING club_id`;
+  const values = [club_id, club_name, description, logo];
+  try {
+    const result = await db.query(query, values);
+    return result.rows[0];
+  } catch (error) {
+    console.error("Error creating club:", error);
+  }
+}
