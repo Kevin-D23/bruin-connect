43a5389c0fc7b8d52a002e731666b90dfcd75974
Kevin-D23 <116064184+Kevin-D23@users.noreply.github.com>
2024-11-23 20:52:52 -0800

    feat: message queries added

--- src/server/queries/messageQueries.ts
+++ src/server/queries/messageQueries.ts
@@ -3,7 +3,7 @@
 // 	sender_id VARCHAR(255) NOT NULL,
 // 	receiver_id VARCHAR(255) NOT NULL,
 // 	content TEXT NOT NULL,
-// 	sent_at TIMESTAMP DEFAULT NOW(),
+// 	created_at TIMESTAMP DEFAULT NOW(),
 // 	CONSTRAINT fk_sender
 // 		FOREIGN KEY (sender_id)
 // 			REFERENCES "user"(user_id),
@@ -11,3 +11,49 @@
 // 		FOREIGN KEY (receiver_id)
 // 			REFERENCES "user"(user_id)
 // );
+
+import db from "../db";
+
+type Message = {
+  message_id: String;
+  sender_id: String;
+  receiver_id: String;
+  content: String;
+};
+
+// Get user by id
+// Return:
+// {
+// message_id: String;
+// sender_id: String;
+// receiver_id: String;
+// content: String;
+// }
+export async function getUserById({ message_id }: Message) {
+  const query = `SELECT message_id, sender_id, receiver_id, content, created_at FROM message WHERE message_id = ($1)`;
+  const values = [message_id];
+
+  try {
+    const result = await db.query(query, values);
+    return result.rows[0];
+  } catch (error) {
+    console.error("Error getting message:", error);
+  }
+}
+
+// Creates a user instance
+export async function createMessage({
+  message_id,
+  sender_id,
+  receiver_id,
+  content,
+}: Message) {
+  const query = `INSERT INTO message (message_id, sender_id, receiver_id, content) VALUES ($1, $2, $3, $4) RETURNING message_id`;
+  const values = [message_id, sender_id, receiver_id, content];
+  try {
+    const result = await db.query(query, values);
+    return result.rows[0];
+  } catch (error) {
+    console.error("Error creating message:", error);
+  }
+}
